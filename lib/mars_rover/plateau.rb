2001315c66fb1d6049ef7049a25dfb5bbefcb3b1
Chirag Aggarwal <me@chi6rag.net>
2015-10-29 11:48:17 +0530

    PlateauSpec: Adding spec to expect #new with invalid params to raise Exception

--- lib/mars_rover/plateau.rb
+++ lib/mars_rover/plateau.rb
@@ -1,8 +1,16 @@
 module MarsRover
   class Plateau
     def initialize(length, breadth)
+      validates_positivity_of length, breadth
       @length = length
       @breadth = breadth
     end
+
+    private
+    def validates_positivity_of length, breadth
+      raise Exception.new 'Wrong Length or Breadth ' +
+                          'Combination' unless (length > 0 && breadth > 0)
+    end
+
   end
 end
--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -4,11 +4,20 @@ module MarsRover
   RSpec.describe Plateau do
 
     describe '#new' do
-      context 'with valid length and breadth' do
+      context 'with valid params' do
         it 'creates a new plateau' do
           p = Plateau.new(5,5)
           expect(p.instance_eval("@length")).to eq(5)
           expect(p.instance_eval("@breadth")).to eq(5)
+        end
+      end
+
+      context 'with invalid params' do
+        it 'raises Exception' do
+          p = nil
+          expect { p = Plateau.new(-10,20) }.to raise_error('Wrong Length or Breadth Combination')
+          expect(p.instance_eval('@length')).to be_nil
+          expect(p.instance_eval('@breadth')).to be_nil
         end
       end
     end
